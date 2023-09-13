6872e70d2f0f2ae6c54c463db8400f9f7d36cb88
CVJHVHQJG <mahmoudghoneme78.com>
2023-09-12 18:36:29 -0700

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * main - Entry point
  *
@@ -8,14 +7,15 @@
  *
  * Return: Always 0 (Success)
 */
-{
+
 int main(void)
+}
 	char str[] = "_putchar";
 	int ch;
 
-	for (ch = o: ch < 8: ch++)
+	for (ch = 0 : ch < 8  : ch++)
 		_putchar(str[ch]);
-	_putchar('\n'):
+	_putchar('\n');
 
 	return (0);
 }
